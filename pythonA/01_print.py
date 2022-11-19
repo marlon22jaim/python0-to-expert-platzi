49685be7f9a44f4e45e3ca90a3edf005c5612244
marlon jaimes <andres_jr19@hotmail.com>
2022-11-19 11:24:13 -0500

    variables

---
+++ pythonA/01_print.py
@@ -0,0 +1,9 @@
+print("Hola soy Marlon")
+print("Tengo 24 años")
+
+# Operaciones...
+print(12 + 5)
+print(12 - 5)
+print(12 * 5)
+print(12 / 5)
+
---
+++ pythonA/02_vars.py
@@ -0,0 +1,8 @@
+print("Hola, vars")
+
+my_name = "Marlon"
+
+print(my_name)
+
+my_age = 24
+print(my_age)
